7261228e1b1fc3a9d6aa33bfbf0035e0c8e00cf4
dabiegorden <117079883+dabiegorden@users.noreply.github.com>
2024-10-17 11:59:30 +0000

    commit

---
+++ frontend/app/page.jsx
@@ -0,0 +1,13 @@
+import LeftSideBar from "@/components/leftSidebar";
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Home</h1>
+      <LeftSideBar />
+    </>
+  );
+};
+
+export default Home;
